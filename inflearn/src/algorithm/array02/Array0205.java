94d9587bc22d35bbb86182712f91a1e4486052b7
kys <kys@DESKTOP-AHBRAEM>
2023-01-05 17:06:43 +0900

    수소(에라토스테네스 체)

--- inflearn/src/algorithm/array02/Array0205.java
+++ inflearn/src/algorithm/array02/Array0205.java
@@ -8,6 +8,7 @@ public class Array0205 {
 	/*자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
 
 	만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.*/
+	
   public int solution (int num){
 		
 		int answer=0;
