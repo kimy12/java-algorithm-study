92c71851db456ef86c43c076d45f10be685758ab
YusinKim <shinkim7941@gmail.com>
2023-01-17 18:46:06 +0900

    03-02. 공통원소 구하기 및 03-01 수정

---
+++ inflearn/src/algorithm/twoPointers03/twoPointers0301.java
@@ -0,0 +1,50 @@
+package algorithm.twoPointers03;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class twoPointers0301 {
+	
+	/*오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.*/
+	
+	public ArrayList<Integer> solution (int num1, int[] arr1, int num2, int[] arr2){
+		
+		ArrayList<Integer> answer = new ArrayList<>();
+		int p1=0, p2=0;
+		
+		while(p1<num1 && p2<num2) {
+			if(arr1[p1] < arr2[p2]) answer.add(arr1[p1++]);
+			else answer.add(arr2[p2++]);
+		}
+		
+		while(p1<num1) answer.add(arr1[p1++]);
+		while(p2<num2) {
+			answer.add(arr2[p2++]);
+		}
+		
+		return answer;
+	}
+	
+	public static void main (String[] args) {
+		
+		twoPointers0301 m = new twoPointers0301();
+		Scanner scan = new Scanner(System.in);
+		
+		int num1 = scan.nextInt();
+		int[] arr1 = new int[num1];
+		
+		for(int i=0; i<num1; i++) {
+			arr1[i] = scan.nextInt();
+		}
+		int num2 = scan.nextInt();
+		int[] arr2 = new int[num2];
+		for(int i=0; i<num2; i++) {
+			arr2[i] = scan.nextInt();
+		}
+		
+		ArrayList<Integer> answer = m.solution(num1, arr1, num2, arr2);
+		for(int i=0; i < num1+num2; i++) {
+			System.out.print(answer.get(i)+" ");
+		}
+	}
+}
--- inflearn/src/algorithm/twoPointers03/twoPointers0302.java
+++ inflearn/src/algorithm/twoPointers03/twoPointers0302.java
@@ -1,30 +1,27 @@
 package algorithm.twoPointers03;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class twoPointers0302 {
 	
-	/*오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.*/
-	/* 3
-		1 3 5
-		5
-		2 3 6 7 9
-	*/
+	/*A, B 두 개의 집합이 주어지면 두 집합의 공통 원소를 추출하여 오름차순으로 출력하는 프로그램을 작성하세요.*/
 	
 	public ArrayList<Integer> solution (int num1, int[] arr1, int num2, int[] arr2){
 		
 		ArrayList<Integer> answer = new ArrayList<>();
 		int p1=0, p2=0;
 		
-		while(p1<num1 && p2<num2) {
-			if(arr1[p1] < arr2[p2]) answer.add(arr1[p1++]);
-			else answer.add(arr2[p2++]);
-		}
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
 		
-		while(p1<num1) answer.add(arr1[p1++]);
-		while(p2<num2) {
-			answer.add(arr2[p2++]);
+		while(p1<num1 && p2<num2) {
+			if(arr1[p1] < arr2[p2]) p1++;
+			else if (arr1[p1] == arr2[p2]) {
+				answer.add(arr1[p1++]);
+				p2++;
+			} else p2++;
 		}
 		
 		return answer;
@@ -32,7 +29,7 @@ public class Main {
 	
 	public static void main (String[] args) {
 		
-		Main m = new Main();
+		twoPointers0302 m = new twoPointers0302();
 		Scanner scan = new Scanner(System.in);
 		
 		int num1 = scan.nextInt();
@@ -48,7 +45,7 @@ public class Main {
 		}
 		
 		ArrayList<Integer> answer = m.solution(num1, arr1, num2, arr2);
-		for(int i=0; i < num1+num2; i++) {
+		for(int i=0; i < answer.size(); i++) {
 			System.out.print(answer.get(i)+" ");
 		}
 	}
